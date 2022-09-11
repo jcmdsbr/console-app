5048a51feec87aa4466bdf5b08bb936af1e02cde
jcmdsbr <jean.carlos@5by5.com.br>
2022-09-11 20:05:01 -0300

    arrumado nome

--- ConsoleAppGit/Carro.cs
+++ ConsoleAppGit/Carro.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleAppGit
+namespace ConsoleAppGit;
+
+internal class Carro
 {
-    internal class carro
-    {
-    }
 }
